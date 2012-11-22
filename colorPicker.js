4bf62b694f840a5b65e10290c4fc0ce15ce769f5
Ryan O'Connell <rymocon@gmail.com>
2012-11-22 03:42:56 -0500

    up

--- colorPicker.js
+++ colorPicker.js
@@ -1,8 +1,5 @@
 jQuery (function () {
 
-var exits = "<p class='exits'><a title='Exits'><i class='icon-share'></i></p>"
-jQuery ('ul.nav').before (exits);
-
 jQuery ('#body').change (function () {
 	jQuery ('body').css ('background-color', jQuery ('#body').val ());
 });
