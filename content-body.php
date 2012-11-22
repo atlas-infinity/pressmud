ab4ef033bc218a9e5181ea7629e1b13433c87446
Ryan O'Connell <rymocon@gmail.com>
2012-11-22 02:58:44 -0500

    up

--- content-body.php
+++ content-body.php
@@ -1,1 +1,30 @@
-<?php the_content () ?>
+<?php
+the_content ();
+$getCustom = get_post_custom ();
+$custom = array ();
+$order = array (
+	'Up',
+	'North',
+	'West',
+	'East',
+	'South',
+	'Down'
+);
+foreach ($order as $key):
+	if (array_key_exists ($key, $getCustom)):
+		$custom [$key] = $getCustom [$key];
+		unset ($getCustom [$key]);
+	endif;
+endforeach;
+
+if (! empty ($custom)): ?>
+	<ul class='roomNavigation'>
+	<?php foreach ($custom as $name => $value):
+		if ($name [0] == '_') continue; // meta data like _edit_lock, _wp_page_template
+		$value = $value [0];
+		$link = get_permalink ($value);
+		?>
+		<li><a href='<?= $link ?>'><?= $name ?></a></li>
+	<?php endforeach ?>
+	</ul> <!-- / roomNavigation -->
+<?php endif ?>
--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
 $ = jQuery
 $ ->
 	exits = "<p class='exits'><a title='Exits'><i class='icon-sitemap'></i></p>"
-	$('ul.nav').before exits
+	$('ul.roomNavigation').before exits
