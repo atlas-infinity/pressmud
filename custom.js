4858d79c9cc0425dbb554fcd8c1311afb914c05a
Ryan O'Connell <rymocon@gmail.com>
2012-11-22 03:40:51 -0500

    up

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 $ = jQuery
 $ ->
 
-	exits = "<aside class='exits'><a rel='tooltip' title='A list of exits from this room'><i class='icon-sitemap'></i></aside>"
+	exits = "<aside class='exits'><a rel='tooltip' data-placement='right' title='A list of exits from this room'><i class='icon-sitemap'></i></aside>"
 	$('ul.roomNavigation').before exits
 
 	$('[rel="tooltip"]').tooltip
--- sub-header.php
+++ sub-header.php
@@ -1,5 +1,5 @@
 <div class='container'>
 <div class='span6 offset3'>
 <nav>
-<a rel="tooltip" title="Return to the entrance" href="<?= site_url () ?>" class="home"><i class="icon-home"></i></a>
+	<a rel="tooltip" data-placement='left' title="Return to the entrance" href="<?= site_url () ?>" class="home"><i class="icon-home"></i></a>
 </nav>
