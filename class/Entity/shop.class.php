18f9aabf40eeb71770a90529008485f35db9f14d
Ryan O'Connell <rymocon@gmail.com>
2012-11-26 20:25:52 -0500

    Controller halfway working

--- class/Entity/shop.class.php
+++ class/Entity/shop.class.php
@@ -4,11 +4,13 @@ require_once (dirname (__FILE__) . '/../entity.class.php');
 class Shop extends Entity {
 
 	public function __construct ($location) {
+		error_log ("construct shop");
 		parent :: __construct ($location);
 		$this -> commands ['listGoods'] = 'List goods for sale';
 	} // construct ()
 
 	protected function listGoods () {
+		error_log ("entity listGoods");
 		return 'test';
 	} // listGoods ()
 
